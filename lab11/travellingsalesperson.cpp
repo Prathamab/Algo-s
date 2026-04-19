d6ed487af7b11f98689d7e2427a9c1d51a770a78
Prathamab <prathamsharmadnn2@gmail.com>
2026-04-19 21:01:06 +0530

    Add lab11

---
+++ lab11/travellingsalesperson.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int n = 4;
+int dist[4][4] = {
+    {0, 10, 15, 20},
+    {10, 0, 35, 25},
+    {15, 35, 0, 30},
+    {20, 25, 30, 0}
+};
+
+int dp[16][4];
+
+int tsp(int mask, int pos) {
+    if (mask == (1 << n) - 1)
+        return dist[pos][0];
+
+    if (dp[mask][pos] != -1)
+        return dp[mask][pos];
+
+    int ans = INT_MAX;
+
+    for (int city = 0; city < n; city++) {
+        if ((mask & (1 << city)) == 0) {
+            int newAns = dist[pos][city]+
+            tsp(mask | (1 << city), city);
+            
+            ans = min(ans, newAns);
+        }
+    }
+
+    return dp[mask][pos] = ans;
+}
+
+int main() {
+    for(int i=0;i<16;i++)
+        for(int j=0;j<4;j++)
+            dp[i][j] = -1;
+
+    cout << "Minimum cost: " << tsp(1, 0);
+    return 0;
+}
